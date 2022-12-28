1d31a1b637bedd0d3e01188edd39876fe9534045
devaskim <dev.devaskim@gmail.com>
2022-12-28 17:10:51 +0500

    Updates

---
+++ precache-manifest.889da22d629e3c3504aa9dda7efc2dbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d916378180ec2a07ae1",
+    "url": "/static/css/main.bcc3923f.chunk.css"
+  },
+  {
+    "revision": "1d916378180ec2a07ae1",
+    "url": "/static/js/main.1d916378.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "272d248eca6d5feb836195f5e89a495f",
+    "url": "/index.html"
+  }
+];
